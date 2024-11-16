158287f9397cb5c718c7979feb3229a951c6e935
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-11-16 06:15:54 +0000

    Data type in js

--- codeChai/day3.js
+++ codeChai/day3.js
@@ -2,3 +2,11 @@
 
 let x= null
 console.log(typeof x)
+
+//number 2 to power 53
+// String
+// boolean true/false
+// null Object
+// undefined  undefined
+// BigInt
+
---
+++ codeChai/dy2.js
@@ -0,0 +1,11 @@
+const accountId = [  1444553]
+var pass="1234"
+let accountEmail="hemantdixit@gmail.com"
+city="Mathura"
+
+// accountId=1
+// console.log(city);
+// console.log(accountEmail);
+
+// to print in table form
+ console.table([accountEmail,accountId,pass,city])
