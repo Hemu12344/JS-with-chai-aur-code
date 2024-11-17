68508338729ee18c93ff4cb5bf043de98e2c9203
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-11-17 15:53:00 +0000

    Day4 Type conversion with notes

---
+++ codeChai/day4.js
@@ -0,0 +1,32 @@
+// let score = true
+
+// console.log(typeof(score));
+
+// let scoreNumber =Number(score)
+// console.log(typeof(scoreNumber));
+// console.log(scoreNumber)
+
+// "33" => 33
+// "33aa" => NaN
+// true =>1 ; false => 0
+
+
+// let isLoggedIn = "Hemant"
+// let isBool = Boolean(isLoggedIn)
+
+// console.log(typeof isBool)
+// console.log(isBool)
+
+// 1 = true ; 0 =false
+
+// "" =false
+// "Hemant" = True
+
+// let floot = 45.5
+// console.log(typeof floot)
+// let floatNumber = Number(floot)
+
+// console.log(typeof floatNumber)
+// console.log(floatNumber)
+
+// 45.5 decimal number alway a number js not a type of datatype like float 
