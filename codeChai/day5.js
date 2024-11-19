31b756e7ae45faa2261e5c5e8411a83a061a1498
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-11-19 15:43:23 +0000

    Operations in javaS

---
+++ codeChai/day5.js
@@ -0,0 +1,12 @@
+// Operations in java Script
+// let a = 5;
+// let b= 3;
+
+// console.log(a%b)
+
+// console.log("1"+1) => 11 because it mean fcfs first come first solve
+
+// console.log("1"+2+3) => 123
+
+// console.log(1+2+"3") => 33
+
