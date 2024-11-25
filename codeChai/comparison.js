a63254f3a7b63584f2570cef0e9ca23dd9588760
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-11-25 17:14:19 +0000

    Data Type : primitive or Non-Primitive

---
+++ codeChai/comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+
+// always work on clean code
+
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null>=0);
+
+
+// console.log(undefined>0);
+// console.log(undefined == 0);
+// console.log(undefined>=0);
+
+// === check strictily
+
+console.log("2"===2);
