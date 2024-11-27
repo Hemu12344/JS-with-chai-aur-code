24cb7005c21d5c79f5b338812e051ffb799910b6
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-11-27 18:12:17 +0000

    Memory allocation Heap & stack

---
+++ codeChai/day6.js
@@ -0,0 +1,42 @@
+// Primitive ==>>
+// 7 type : String , number , boolean , null , undefined , Symbol , bigInt  
+
+// non - premitive 
+
+// 3 type : Array , object ,Function
+
+
+// const Score = 10
+// const ScoreVal =10.5
+
+// const isLogged = false
+// const ooutSide= null
+// let user;
+
+// const id =Symbol('123')
+
+// const anotherId=Symbol('123')
+
+// const bigint=2456325632156325632156n
+
+// console.log(bigint)
+
+
+// Array ,object,Function
+
+const arr =["Ram","Shyam",45]
+let myDetail={
+    Name:"Hemant Dixit",
+    Age:18,
+}
+
+const fun = function(){
+    return "Hello World"
+}
+
+
+console.log(arr)
+console.log(myDetail["Name"])
+let a = fun()
+
+console.log(a)
---
+++ codeChai/day7.js
@@ -0,0 +1,25 @@
+"use strictly";
+// Memory allocation Heap  & stack
+
+// Heap => it return orginal value , also it store non-premitive data
+// Stack => it return copy value , also it store premitive data
+
+// Example => Stack 
+
+// let name ="Hemant"
+
+// let anothername =name
+// anothername = 'Ram'
+// console.log(name)
+// console.log(anothername)
+
+// Example => Heap    
+
+// let Emp = {
+//     name:"Hemant",
+//     Salary:1000,
+//     Age :18
+// }
+// let check_Name = `welcome ${Emp.name} to the coding sesion with chai aur code`
+
+// console.log(check_Name)
