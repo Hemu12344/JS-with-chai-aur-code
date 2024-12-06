dbd67c58a6f814e81dd7d94951e52f7b3e99b025
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-12-06 15:56:25 +0000

    Math or Number math With Random

---
+++ codeChai/day8.js
@@ -0,0 +1,24 @@
+// Number and Math in Jsc
+
+// const score = 400
+
+// const bal = new Number (100)
+// console.log(bal.toString().toLocaleUpperCase)
+// console.log(score)
+
+// let num = 10000000
+// console.log()
+// console.log(num.toLocaleString('en-in'))
+
+// ======================== Maths =============================
+
+// console.log(Math.abs())
+
+// console.log(Math.round(456.4))
+
+// console.log(Math.random(1))
+
+let min =1
+let max =6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
