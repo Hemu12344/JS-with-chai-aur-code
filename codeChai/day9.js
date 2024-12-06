a6b1113cfbe1d80c0f71d72ef1cadc9ba67781e9
Hemant Dixit <159431982+Hemu12344@users.noreply.github.com>
2024-12-06 16:18:34 +0000

    DateTime

---
+++ codeChai/day9.js
@@ -0,0 +1,16 @@
+// Date 
+let date = new Date("2025-05-15")
+
+// console.log(date.toDateString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toJSON())
+// console.log(typeof(date))
+// console.log(date.toString())
+
+// console.log(date.getDate())
+// console.log(date.getDay())
+
+console.log(date.toLocaleString())
+// // timeStapm
+
+// let timesStamp = Date.now()
